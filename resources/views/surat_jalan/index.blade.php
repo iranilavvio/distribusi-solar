131bf8f762d7fd2be382179f878803ffc5d7cfd5
iranilavvio <iranisilma25@gmail.com>
2022-08-02 17:51:12 +0800

    breadcrumb

--- resources/views/surat_jalan/index.blade.php
+++ resources/views/surat_jalan/index.blade.php
@@ -9,8 +9,8 @@
                         <h6 class="h2 text-white d-inline-block mb-0">Surat Jalan</h6>
                         <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                             <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
-                                <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
-                                <li class="breadcrumb-item"><a href="#">Surat Jalan</a></li>
+                                <li class="breadcrumb-item"><a href="{{ route('home') }}"><i class="fas fa-home"></i></a>
+                                </li>
                                 <li class="breadcrumb-item active" aria-current="page">Surat Jalan</li>
                             </ol>
                         </nav>
